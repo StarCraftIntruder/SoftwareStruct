0b4871ec18befc9c5be869033349b0cac5265f5f
zhendery <zhendery@qq.com>
2016-01-04 23:17:14 +0800

    修改了 背景移动~之前那一版会跳页

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour {
+    public Vector3 vec;
+	void Update () {
+        transform.Rotate(vec, Space.Self);
+	}
+}
